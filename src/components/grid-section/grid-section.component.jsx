b8d9d2c22169171f8dfe6af9b1b85aba5cd45048
luke john <luke0145555@hotmail.co.uk>
2020-08-02 10:26:09 +0100

    made grid-section component, will change based on slider position

---
+++ src/components/grid-section/grid-section.component.jsx
@@ -0,0 +1,17 @@
+import React, { useEffect, useState } from 'react';
+import './grid-section.styles.scss';
+
+const GridSection = ({sliderNumber}) => {
+
+    return (
+        <div className='grid-section-container'>
+            <div className={`grid-item ${sliderNumber === 1 ? 'blue' : ''}`}></div>
+            <div className={`grid-item ${sliderNumber === 1 ? 'green' : ''}`}></div>
+            <div className={`grid-item ${sliderNumber === 1 ? 'red' : ''}`}></div>
+            <div className={`grid-item ${sliderNumber === 1 ? 'orange' : ''}`}></div>
+            <div className={`grid-item ${sliderNumber === 1 ? 'purple' : ''}`}></div>
+        </div>
+    )
+}
+
+export default GridSection;
